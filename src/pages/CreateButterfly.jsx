5043333e3d9a25a2dbadf016605877611a460400
Aday25 <aday.alvarez84@gmail.com>
2025-07-19 11:29:11 +0200

    setup imports and image constants

--- src/pages/CreateButterfly.jsx
+++ src/pages/CreateButterfly.jsx
@@ -1,9 +1,17 @@
-const CreateButterfly = () => {
-  return (
-    <>
-    <p>Hola estoy en newbutterfly</p>
-    </>
-  )
-}
+import { useState } from "react";
+import "./CreateButterfly.css";
 
-export default CreateButterfly;
+// Array con las imágenes que muestran el progreso según campos rellenados
+const progressImages = [
+  "/images-form/oruga1.png",
+  "/images-form/oruga2.png",
+  "/images-form/oruga3.png",
+  "/images-form/oruga4.png",
+  "/images-form/mariposa1.png",
+  "/images-form/mariposa2.png",
+  "/images-form/mariposa3.png",
+];
+
+const migratoryImage = "/images-form/migratoria.png";
+const savedImage = "/images-form/guardada1.png";
+
