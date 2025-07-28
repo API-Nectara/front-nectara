43f5ddbb7fcb5a77f8bacec724fed3e2e2102b0e
sofiareyes12 <121201sofiareyes@gmail.com>
2025-07-28 10:18:06 +0200

    color change in nuestro equipo & name

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
       github: "https://github.com/angiepereir"
     },
     {
-      name: "Ana Murzaban",
+      name: "Ana Muruzabal",
       image: "/images-form/ana.jpg",
       bio: "Diseñadora UI/UX enfocada en biodiversidad y experiencias educativas.",
       linkedin: "https://www.linkedin.com/in/ana-muruzabal-1ab453209/",
@@ -24,7 +24,7 @@ const AboutUs = () => {
       github: "https://github.com/Sofiareyes12"
     },
     {
-      name: "Aday la mas guapa.",
+      name: "Aday Alvarez",
       image: "/images-form/aday.jpg.webp",
       bio: "Diseñadora UI/UX enfocada en biodiversidad y experiencias educativas.",
       linkedin: "https://www.linkedin.com/in/adayasc/",
@@ -34,7 +34,7 @@ const AboutUs = () => {
 
   return (
     <div className="min-h-screen bg-green-50 py-16 px-6">
-      <h1 className="text-4xl font-bold text-center text-green-800 mb-12">Nuestro equipo</h1>
+      <h1 className="text-4xl font-bold text-center text-[#e66035] mb-12">Nuestro equipo</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {team.map((member, index) => (
           <TeamCard key={index} {...member} />
