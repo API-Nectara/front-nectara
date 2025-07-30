022652f7fd81ba1375df137bd3566980417e030d
angie <pereiraanngy@gmail.com>
2025-07-30 16:37:49 +0200

    about

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -53,7 +53,7 @@ const AboutUs = () => {
       >
         Nuestro equipo
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 max-w-6xl mx-auto">
         {team.map((member, index) => (
           <TeamCard key={index} {...member} />
         ))}
